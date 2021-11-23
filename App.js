f8b080c34f076d1a2bb663e4778f2f7f18eeb922
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-11-23 03:43:05 -0500

    map.map

--- App.js
+++ App.js
@@ -10,12 +10,14 @@ import {
 } from "react-native";
 import bg from "./assets/bg.jpeg";
 
+const emptyMap = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
 export default function App() {
-  const [map, setMap] = useState([
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ]);
+  const [map, setMap] = useState();
 
   const [currentTurn, setCurrentTurn] = useState("x");
 
@@ -26,21 +28,137 @@ export default function App() {
     }
     setMap((existingMap) => {
       const updatedMap = [...existingMap];
-      updatedMap[(rowIndex[columnIndex] = currentTurn)];
-      return existingMap;
+      updatedMap[rowIndex][columnIndex] = currentTurn;
+      return updatedMap;
     });
 
     setCurrentTurn(currentTurn === "x" ? "o" : "x");
+
+    const winner = getWinner();
+    if (winner) {
+      gameWon(winner);
+    } else {
+      checkTieState();
+    }
+  };
+
+  const getWinner = () => {
+    // check rows
+
+    for (let i = 0; i < 3; i++) {
+      const isRowXWinning = map[i].every((cell) => cell === "x");
+      const isRowOWinning = map[i].every((cell) => cell === "o");
+
+      if (isRowXWinning) {
+        return "x";
+      }
+      if (isRowOWinning) {
+        return "o";
+      }
+    }
+
+    // check columns
+
+    for (let col = 0; col < 3; col++) {
+      let isColumnXWinner = true;
+      let isColumnOWinner = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (map[row][col] !== "x") {
+          isColumnXWinner = false;
+        }
+        if (map[row][col] !== "o") {
+          isColumnOWinner = false;
+        }
+      }
+
+      if (isColumnXWinner) {
+        return "x";
+        break;
+      }
+      if (isColumnOWinner) {
+        return "o";
+        break;
+      }
+    }
+
+    let isDiagonal1OWinning = true;
+    let isDiagonal1XWinning = true;
+    let isDiagonal2OWinning = true;
+    let isDiagonal2XWinning = true;
+    for (let i = 0; i < 3; i++) {
+      if (map[i][i] !== "o") {
+        isDiagonal1OWinning = false;
+      }
+      if (map[i][i] !== "x") {
+        isDiagonal1XWinning = false;
+      }
+      if (map[i][2 - i] !== "o") {
+        isDiagonal2OWinning = false;
+      }
+      if (map[i][2 - i] !== "x") {
+        isDiagonal2XWinning = false;
+      }
+    }
+
+    if (isDiagonal1OWinning || isDiagonal2OWinning) {
+      return "o";
+    }
+    if (isDiagonal1XWinning || isDiagonal2XWinning) {
+      return "x";
+    }
+  };
+
+  const checkTieState = () => {
+    if (!map.some((row) => row.some((cell) => cell === ""))) {
+      Alert.alert(`It's a tie`, `tie`, [
+        {
+          text: "Restart",
+          onPress: resetGame,
+        },
+      ]);
+    }
+  };
+
+  const gameWon = (player) => {
+    Alert.alert(`Hurrrayy`, `Player ${player} won`, [
+      {
+        text: "Restart",
+        onPress: resetGame,
+      },
+    ]);
+  };
+
+  const resetGame = () => {
+    setMap([
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]);
+
+    setCurrentTurn("x");
   };
 
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
+        <Text
+          style={{
+            fontSize: 24,
+            color: "white",
+            marginBottom: "auto",
+            marginTop: 50,
+          }}
+        >
+          {" "}
+          Current Turn : {currentTurn.toUpperCase()}{" "}
+        </Text>
         <View style={styles.map}>
           {map.map((row, rowIndex) => (
-            <View style={styles.row}>
+            <View key={`row-${rowIndex}`} style={styles.row}>
               {row.map((cell, columnIndex) => (
                 <Pressable
+                  key={`row-${rowIndex}-col-${columnIndex}`}
                   onPress={() => onPress(rowIndex, columnIndex)}
                   style={styles.cell}
                 >
@@ -89,8 +207,6 @@ const styles = StyleSheet.create({
     paddingTop: 15,
   },
   map: {
-    borderWidth: 1,
-    borderColor: "white",
     width: "80%",
     aspectRatio: 1,
   },
@@ -102,9 +218,6 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     flex: 1,
-
-    borderWidth: 1,
-    borderColor: "white",
   },
 
   circle: {
