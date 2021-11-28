ad4bc8b50f237e03ebcd7913aa3ea5a31fdbd811
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-11-28 05:12:56 -0500

    backend tied

--- App.js
+++ App.js
@@ -1,38 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  Pressable,
-  Alert,
-} from "react-native";
+import { Text, View, ImageBackground, Alert } from "react-native";
 import bg from "./assets/bg.jpeg";
 import Cell from "./src/components/Cell";
+import { emptyMap } from "./src/utils";
+import { getWinner, isTie } from "./src/utils/gameLogic";
+import { botTurn } from "./src/utils/bot";
 
 import Amplify from "@aws-amplify/core";
 import { Auth } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
 import config from "./src/aws-exports";
+import styles from "./App.style";
 
 Amplify.configure(config);
-
-const emptyMap = [
-  ["", "", ""], // 1st row
-  ["", "", ""], // 2nd row
-  ["", "", ""], // 3rd row
-];
-
-const copyArray = (original) => {
-  console.log("ghe");
-  console.log(original);
-  const copy = original.map((arr) => {
-    return arr.slice();
-  });
-  console.log(copy);
-  return copy;
-};
 
 function App() {
   const [map, setMap] = useState(emptyMap);
@@ -41,7 +22,10 @@ function App() {
 
   useEffect(() => {
     if (currentTurn === "o" && gameMode !== "LOCAL") {
-      botTurn();
+      const chosenOption = botTurn(map, gameMode);
+      if (chosenOption) {
+        onPress(chosenOption.row, chosenOption.col);
+      }
     }
   }, [currentTurn, gameMode]);
 
@@ -73,73 +57,8 @@ function App() {
     Auth.signOut();
   };
 
-  const getWinner = (winnerMap) => {
-    // Check rows
-    for (let i = 0; i < 3; i++) {
-      const isRowXWinning = winnerMap[i].every((cell) => cell === "x");
-      const isRowOWinning = winnerMap[i].every((cell) => cell === "o");
-
-      if (isRowXWinning) {
-        return "x";
-      }
-      if (isRowOWinning) {
-        return "o";
-      }
-    }
-
-    // Check columns
-    for (let col = 0; col < 3; col++) {
-      let isColumnXWinner = true;
-      let isColumnOWinner = true;
-
-      for (let row = 0; row < 3; row++) {
-        if (winnerMap[row][col] !== "x") {
-          isColumnXWinner = false;
-        }
-        if (winnerMap[row][col] !== "o") {
-          isColumnOWinner = false;
-        }
-      }
-
-      if (isColumnXWinner) {
-        return "x";
-      }
-      if (isColumnOWinner) {
-        return "o";
-      }
-    }
-
-    // check diagonals
-    let isDiagonal1OWinning = true;
-    let isDiagonal1XWinning = true;
-    let isDiagonal2OWinning = true;
-    let isDiagonal2XWinning = true;
-    for (let i = 0; i < 3; i++) {
-      if (winnerMap[i][i] !== "o") {
-        isDiagonal1OWinning = false;
-      }
-      if (winnerMap[i][i] !== "x") {
-        isDiagonal1XWinning = false;
-      }
-
-      if (winnerMap[i][2 - i] !== "o") {
-        isDiagonal2OWinning = false;
-      }
-      if (winnerMap[i][2 - i] !== "x") {
-        isDiagonal2XWinning = false;
-      }
-    }
-
-    if (isDiagonal1OWinning || isDiagonal2OWinning) {
-      return "o";
-    }
-    if (isDiagonal1XWinning || isDiagonal2XWinning) {
-      return "x";
-    }
-  };
-
   const checkTieState = () => {
-    if (!map.some((row) => row.some((cell) => cell === ""))) {
+    if (isTie(map)) {
       Alert.alert(`It's a tie`, `tie`, [
         {
           text: "Restart",
@@ -165,61 +84,6 @@ function App() {
       ["", "", ""], // 3rd row
     ]);
     setCurrentTurn("x");
-  };
-
-  const botTurn = () => {
-    // collect all possible options
-    const possiblePositions = [];
-    map.forEach((row, rowIndex) => {
-      row.forEach((cell, columnIndex) => {
-        if (cell === "") {
-          possiblePositions.push({ row: rowIndex, col: columnIndex });
-        }
-      });
-    });
-
-    let chosenOption;
-
-    if (gameMode === "BOT_MEDIUM") {
-      // Attack
-      possiblePositions.forEach((possiblePosition) => {
-        const mapCopy = copyArray(map);
-
-        mapCopy[possiblePosition.row][possiblePosition.col] = "o";
-
-        const winner = getWinner(mapCopy);
-        if (winner === "o") {
-          // Attack that position
-          chosenOption = possiblePosition;
-        }
-      });
-
-      if (!chosenOption) {
-        // Defend
-        // Check if the opponent WINS if it takes one of the possible Positions
-        possiblePositions.forEach((possiblePosition) => {
-          const mapCopy = copyArray(map);
-
-          mapCopy[possiblePosition.row][possiblePosition.col] = "x";
-
-          const winner = getWinner(mapCopy);
-          if (winner === "x") {
-            // Defend that position
-            chosenOption = possiblePosition;
-          }
-        });
-      }
-    }
-
-    // choose random
-    if (!chosenOption) {
-      chosenOption =
-        possiblePositions[Math.floor(Math.random() * possiblePositions.length)];
-    }
-
-    if (chosenOption) {
-      onPress(chosenOption.row, chosenOption.col);
-    }
   };
 
   return (
@@ -297,48 +161,5 @@ function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#242D34",
-  },
-  bg: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-
-    paddingTop: 15,
-  },
-  map: {
-    width: "80%",
-    aspectRatio: 1,
-  },
-  row: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  buttons: {
-    position: "absolute",
-    bottom: 50,
-  },
-  logout: {
-    alignSelf: "center",
-    color: "white",
-    fontSize: 16,
-  },
-  button: {
-    color: "white",
-    margin: 10,
-    fontSize: 16,
-    backgroundColor: "#191F24",
-    padding: 10,
-    paddingHorizontal: 15,
-  },
-});
 
 export default withAuthenticator(App);
